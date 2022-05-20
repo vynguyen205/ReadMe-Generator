d4d915d64925931c8d78b20d3ca0d2c6a4659a64
Vy Nguyen <vynguyen205@Vys-MacBook-Air.local>
2022-05-19 22:31:09 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// TODO: Include packages needed for this application
+var inquirer = require("inquirer")
+var fs = require('fs');
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
